a8d9f3c1179b44192b0a7b41a11ec8dbf9eb445e
Miguel Rivera <miguel@smsysonline.com>
2018-03-13 18:27:09 -0400

    Revert Objects moved to Objects library

---
+++ src/library/tests/test-ca-lang.spec.ts
@@ -0,0 +1,11 @@
+import {
+  CALangModule, ICALangService, CALang
+} from '../ca-lang';
+
+describe('CALang', () => {
+
+  it('files were exported', () => {
+    expect(true).toBeTruthy();
+  });
+
+});
